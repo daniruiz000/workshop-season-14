fcf8e961064833305cd09041a6a3a5c852bfbf7b
Daniel Ruiz <danielruimar@gmail.com>
2023-03-24 23:41:06 +0100

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn ES button', () => {
   render(<App />);
   const text = screen.getByText('ES');
+  expect(text).toBeInTheDocument();
+});
+test('renders learn EN button', () => {
+  render(<App />);
+  const text = screen.getByText('EN');
   expect(text).toBeInTheDocument();
 });
